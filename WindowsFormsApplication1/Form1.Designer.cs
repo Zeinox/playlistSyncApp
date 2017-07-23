4da93ebbeca7d16d0095fde1568b5b5739f5c725
Zeinox <Zeinox.rar@gmail.com>
2017-07-23 16:47:15 -0500

    Playlist Order Drag/Drop
    You can now alter the order of the list by dragging an added song to
    your desired position.

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -29,10 +29,12 @@
         private void InitializeComponent()
         {
             this.playList = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // playList
             // 
+            this.playList.AllowDrop = true;
             this.playList.Dock = System.Windows.Forms.DockStyle.Left;
             this.playList.FormattingEnabled = true;
             this.playList.ItemHeight = 16;
@@ -41,22 +43,37 @@
             this.playList.Size = new System.Drawing.Size(542, 268);
             this.playList.TabIndex = 0;
             this.playList.SelectedIndexChanged += new System.EventHandler(this.playList_SelectedIndexChanged);
+            this.playList.DragDrop += new System.Windows.Forms.DragEventHandler(this.playList_DragDrop);
+            this.playList.DragEnter += new System.Windows.Forms.DragEventHandler(this.playList_DragEnter);
+            this.playList.DragOver += new System.Windows.Forms.DragEventHandler(this.playList_DragOver);
+            this.playList.MouseDown += new System.Windows.Forms.MouseEventHandler(this.playList_MouseDown);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(549, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "playlistName";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(688, 268);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.playList);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox playList;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -16,6 +16,7 @@ namespace WindowsFormsApplication1
     public partial class Form1 : Form
     {
         public string[] validFileExtensions = { ".mp3", ".wav", ".aa", ".aiff", ".flac", ".wma" };
+        public Boolean newSong = false;
 
         public Form1()
         {
@@ -44,9 +45,51 @@ namespace WindowsFormsApplication1
             //more processing
         }
 
+        //Thanks to stackoverflow: https://stackoverflow.com/questions/805165/reorder-a-winforms-listbox-using-drag-and-drop
+        //
+        private void playList_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (this.playList.SelectedItem == null) return;
+            this.playList.DoDragDrop(this.playList.SelectedItem, DragDropEffects.Move);
+        }
+
+        private void playList_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+        }
+
+        private void playList_DragDrop(object sender, DragEventArgs e)
+        {
+            if(newSong)
+            {
+                Form_DragDrop(sender, e);
+                newSong = false;
+                return;
+            }
+
+            Point point = playList.PointToClient(new Point(e.X, e.Y));
+            int index = this.playList.IndexFromPoint(point);
+            if (index < 0) index = this.playList.Items.Count - 1;
+            object data = e.Data.GetData(typeof(string));
+            this.playList.Items.Remove(data);
+            this.playList.Items.Insert(index, data);
+        }
+
+        //
+
+        void playList_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+                newSong = true;
+            }
+        }
+
         private void playList_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
+        
     }
 }
